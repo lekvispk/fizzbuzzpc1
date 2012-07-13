45bec338ccd2cf360dcb3f4f21a0a4ff43a80bcc
git --version <git -version>
2012-07-12 21:21:19 -0500

    segundo test creado

--- FizzBuzz_specs.rb
+++ FizzBuzz_specs.rb
@@ -1,9 +1,18 @@
 require "minitest/autorun"
 require_relative "FizzBuzz"
 
+
 describe FizzBuzz do
+  
   it "#devuelve cuando envio solo un numero" do  			
   			fizzbuzz = FizzBuzz.new
   			fizzbuzz.pintar(1).must_equal "1"
   end
+
+  it "#devuelve cuando envio el numero 2" do  			
+  			fizzbuzz = FizzBuzz.new
+  			fizzbuzz.pintar(2).must_equal "12"
+  end
+
+
 end
