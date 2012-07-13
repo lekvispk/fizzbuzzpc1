374cc95930a91536cecbb5b20b9c113828a96e36
git --version <git -version>
2012-07-12 21:30:09 -0500

    cuarto test, no hizo falta hacer cambios para implementacion

--- FizzBuzz_specs.rb
+++ FizzBuzz_specs.rb
@@ -19,4 +19,8 @@ it "#devuelve cuando envio el numero 3" do
   			fizzbuzz.pintar(3).must_equal "12fizz"
   end
 
+it "#devuelve cuando envio el numero 4" do  			
+  			fizzbuzz = FizzBuzz.new
+  			fizzbuzz.pintar(4).must_equal "12fizz4"
+  end
 end
